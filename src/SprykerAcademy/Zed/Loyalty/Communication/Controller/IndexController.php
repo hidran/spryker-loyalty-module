8f962e9bdb9242d2fd9944cc6e7689f209bc29cd
Hidran Arias <hidran@gmail.com>
2025-06-17 22:59:05 +0200

    Adds LoyaltyTransfers

---
+++ src/SprykerAcademy/Zed/Loyalty/Communication/Controller/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types = 1);
+
+namespace SprykerAcademy\Zed\Loyalty\Communication\Controller;
+
+use Spryker\Zed\Kernel\Communication\Controller\AbstractController;
+
+/**
+ * @method \SprykerAcademy\Zed\Loyalty\Communication\LoyaltyCommunicationFactory getFactory()
+ * @method \SprykerAcademy\Zed\Loyalty\Business\LoyaltyFacadeInterface getFacade()
+ */
+class IndexController extends AbstractController
+{
+}
