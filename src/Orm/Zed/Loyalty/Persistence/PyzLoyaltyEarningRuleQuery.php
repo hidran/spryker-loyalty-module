b1c6739d6c1d81683eadee133ba4201fb700855d
Hidran Arias <hidran@gmail.com>
2025-06-17 20:25:26 +0200

    Adds loyalty tables schema for rules and redeem

---
+++ src/Orm/Zed/Loyalty/Persistence/PyzLoyaltyEarningRuleQuery.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Orm\Zed\Loyalty\Persistence;
+
+use Orm\Zed\Loyalty\Persistence\Base\PyzLoyaltyEarningRuleQuery as BasePyzLoyaltyEarningRuleQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'pyz_loyalty_earning_rule' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class PyzLoyaltyEarningRuleQuery extends BasePyzLoyaltyEarningRuleQuery
+{
+
+}
